922e29dd32c5a24cba66a4074afc6a3f0e0741f0
wrafael91 <wrafaelmp91@gmail.com>
2021-03-17 10:11:17 -0500

    dataserver basics

---
+++ src/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+// Replace this with your MONGOURI.
+const MONGOURI = "mongodb://localhost:27017/database-moviedb?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+
+const InitiateMongoServer = async () => {
+  try {
+    await mongoose.connect(MONGOURI, {
+      useNewUrlParser: true
+    });
+    console.log("Connected to DB !!");
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+module.exports = InitiateMongoServer;
