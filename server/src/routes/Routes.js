74f712380d554d3d0f4e110a52b88b40e7f3bb44
wrafael91 <wrafaelmp91@gmail.com>
2021-03-24 22:19:43 -0500

    using bcrypt to secure password

--- server/src/routes/Routes.js
+++ server/src/routes/Routes.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const signUpTemplateCopy = require('../models/SignUpModels.js')
+const bcrypt = require('bcrypt');
 
+router.post('/signup', async (request, response) => {
 
-router.post('/signup', (request, response) => {
+    //Making the password secure by hashing the characters.
+    const saltPassword = await bcrypt.genSalt(10);
+    const securePassword = await bcrypt.hash(request.body.password, saltPassword);
+
     const { fullname, username, email, password  } = request.body;
     const signedUpUser = new signUpTemplateCopy();
     
     signedUpUser.fullname = fullname;
     signedUpUser.username = username;
     signedUpUser.email = email;
-    signedUpUser.password = password;
+    signedUpUser.password = securePassword;
 
     signedUpUser.save()
     .then(data => {
