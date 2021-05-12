e430022459bd221832faa4205a0a9c37d93e4222
Link Strifer <linkstrifer@outlook.com>
2021-05-12 17:07:32 -0500

    add node env variables

--- server/src/routes/database.js
+++ server/src/routes/database.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
 
-// Setting up MongoDB 
-const serverKeys = require('../config/config.js');
-
-mongoose.connect(serverKeys.MONGOURI_API_KEY, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: true
-})
-.then(db => console.log('MongoDB database connected'))
-.catch(error => console.log('error'));
+mongoose
+  .connect(process.env.MONGOURI_API_KEY, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then((db) => console.log("MongoDB database connected"))
+  .catch((error) => console.log("error"));
 
 module.exports = mongoose;
