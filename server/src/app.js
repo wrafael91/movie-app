8bb533cfd6f579057da14f6b13d5cf3554a28ba9
wrafael91 <wrafaelmp91@gmail.com>
2021-05-15 14:15:56 -0500

    adding dist folder

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 const morgan = require('morgan');
 require("./routes/database.js");
 const cors = require('cors');
-
+const path = require('path');
 // PORT
 app.set('Port', process.env.PORT || 5000);
 
@@ -16,14 +16,12 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors({origen: "*"}));
+app.use(express.static(path.join(__dirname,'dist')));
 
 //Routes
 app.use('/app', require('./routes/index.js'));
 app.use('/app/favorite', require('./routes/favorite.js'));
 
-app.get("/", (request, response) => {
-  response.send("is working");
-});
 
 app.listen(app.get('Port'), () => {
   console.log('Server running on port ', app.get('Port'))
